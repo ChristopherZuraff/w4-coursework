fb80aab1289a4ffd12808a8cc3b125239cf10393
ChristopherZuraff <36679712+ChristopherZuraff@users.noreply.github.com>
2018-06-12 00:31:11 -0700

    Add files via upload

---
+++ Databases/w4l1p1.sql
@@ -0,0 +1,11 @@
+SELECT FirstName  FROM customers
+WHERE FirstName LIKE "Jennifer";
+
+SELECT Name, Composer, AlbumId FROM tracks
+WHERE AlbumId >250 and AlbumId <340;
+
+SELECT FirstName, LastName, Phone, Fax, Email, Country FROM customers
+WHERE Country LIKE "Brazil" OR Country LIKE "USA";
+
+SELECT * FROM albums
+WHERE title LIKE "a%";
